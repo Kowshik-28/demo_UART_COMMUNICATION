3c952d25e49cfc6a741132224bab38eb2737d5eb
BML-AP-177\BOSON-177 <kowshik.kancharla@bosonmotors.com>
2025-05-15 11:34:32 +0530

    Arduino_MEGA_code

---
+++ UART_DUPLEX_COMMUNICATION/UART_DUPLEX_COMMUNICATION.ino
@@ -0,0 +1,19 @@
+uint8_t txByte = 0xA; 
+void setup() {
+  Serial.begin(9600);      // For PC Serial Monitor
+  Serial1.begin(115200);   // Match STM32 baud rate
+}
+
+void loop() {
+   Serial1.write(txByte);     // Send to STM32
+  Serial.print("Sent to STM32: 0x");
+  Serial.println(txByte, HEX);  // Print HEX value to monitor
+
+  delay(1000);
+  if (Serial1.available()) {
+    uint8_t data = Serial1.read();  // Read byte from STM32
+    Serial.print("Received: ");
+    Serial.println(data,HEX);           // Should print: 99 every second
+  }
+  delay(1000);
+}
